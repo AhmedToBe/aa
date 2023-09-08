de8f1c7e88504b911f0251d53502e4456dd991fa
OmarKhalifa00 <omarhany94671@gmail.com>
2023-09-08 15:47:30 -0700

    The last digit

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -1,27 +1,35 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - main
  *
- * Description: print value of n status:
- *		greater than, is zero and is not less than 6.
+ * Description: Description
  *
- * return: 0 (seccess)
+ * Return: Return
 */
 int main(void)
 {
-	int n, digit;
+int n;
+int LD;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	digit = n % 10;
-	if (digit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-	else if (digit == 0)
-		printf("last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit != 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+LD = n % 10;
+
+if (LD > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, LD);
+}
+else if (LD == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, LD);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LD);
+}
+
+return (0);
 }
