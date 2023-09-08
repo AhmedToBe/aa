62fa9f4b753df2d38b5ad59fd3bf27649df13f72
OmarKhalifa00 <omarhany94671@gmail.com>
2023-09-08 16:53:21 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * Description: desc
+ * Description: A
  *
  * Return: 0 (success)
 */
 
 int main(void)
-
-	int = 0;
+{
+	int digit = 0;
 
 	while (digit <= 9)
 	{
@@ -20,3 +19,4 @@ int main(void)
 	printf("\n");
 
 	return (0);
+}
